5a42a5cac9acc83abe9b2ef8bf563ffb25541bdc
ruisun <stevesuncanada@gmail.com>
2017-01-04 15:41:53 -0500

    - Clean up code.

--- utilities-play/src/main/java/org/gogoup/utilities/play/ApplicationLoader.java
+++ utilities-play/src/main/java/org/gogoup/utilities/play/ApplicationLoader.java
@@ -2,6 +2,8 @@ package org.gogoup.utilities.play;
 
 import com.typesafe.config.ConfigFactory;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.Configuration;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.inject.guice.GuiceApplicationLoader;
@@ -13,6 +15,8 @@ import java.util.Date;
  */
 public class ApplicationLoader extends GuiceApplicationLoader {
 
+    private static final Logger LOG = LoggerFactory.getLogger("loader");
+
     @Override
     public GuiceApplicationBuilder builder(play.ApplicationLoader.Context context) {
         String env = System.getProperty("env");
@@ -22,10 +26,10 @@ public class ApplicationLoader extends GuiceApplicationLoader {
         if (StringUtils.isBlank(env)) {
             env = "dev";
         }
-        System.out.println("Environment: " + env);
+        LOG.debug("Environment: {}", env);
         String configPath = String.format("application.%s.conf", env);
-        System.out.println("Configuration: " + configPath);
-        System.out.println("Starting at: " + new Date(System.currentTimeMillis()));
+        LOG.debug("Configuration: {}", configPath);
+        LOG.debug("Starting at: {}", new Date(System.currentTimeMillis()));
         Configuration cfg = new Configuration(ConfigFactory.load(configPath));
         return initialBuilder
                 .in(context.environment())
